af00e79161cdf95f18cbfd25941a1a54e4768194
Simone <Simone@DESKTOP-QJV4D9B>
2019-12-19 12:46:25 +0100

    Add project files.

---
+++ WebServiceTutorial/WebServiceTutorial/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebServiceTutorial
+    {
+        public static class Constants
+        {
+            public const string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+            public const string OpenWeatherMapAPIKey = "c1e6ba031decc7d6dfe0cfc3373bd1db";
+        }
+    }
+
